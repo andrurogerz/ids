58737380d40cc3ae3be7779e59b3de0e0b0e76c7
Andrew Rogers <andrurogerz@gmail.com>
2025-06-03 16:29:45 -0700

    add methods

--- Tests/Functions.hh
+++ Tests/Functions.hh
@@ -34,36 +34,33 @@ int function_7(int);
 __attribute((deprecated("this function is deprecated"))) int function_9(int);
 
 struct Class {
-  // CHECK: Functions.hh:[[@LINE+1]]:80: remark: unexported public interface 'method_1'
-  __attribute__((always_inline)) [[deprecated("this function is deprecated")]] int method_1(int);
+  // CHECK: Functions.hh:[[@LINE+1]]:49: remark: unexported public interface 'method_1'
+  [[deprecated("this function is deprecated")]] int method_1(int);
 
-  // CHECK: Functions.hh:[[@LINE+1]]:61: remark: unexported public interface 'method_2'
-  __attribute__((deprecated("this function is deprecated")))int method_2(int);
+  // CHECK: Functions.hh:[[@LINE+1]]:48: remark: unexported public interface 'method_2'
+  [[deprecated("this function is deprecated")]]int method_2(int);
 
-  // CHECK: Functions.hh:[[@LINE+1]]:57: remark: unexported public interface 'method_3'
-  [[nodiscard]] __attribute__((noinline)) /* comment */ int method_3(int);
+  // CHECK: Functions.hh:[[@LINE+1]]:31: remark: unexported public interface 'method_3'
+  [[nodiscard]] /* comment */ int method_3(int);
 
-  // CHECK: Functions.hh:[[@LINE+1]]:75: remark: unexported public interface 'method_4'
-  __attribute__((noinline)) [[deprecated("this function is deprecated")]] int method_4(int);
-
-  // CHECK: Functions.hh:[[@LINE+2]]:17: remark: unexported public interface 'method_5'
-  [[nodiscard]] __attribute__((deprecated("this function is deprecated")))
-  /* comment */ int method_5(int);
-
-  // CHECK: Functions.hh:[[@LINE+2]]:3: remark: unexported public interface 'method_6'
+  // CHECK: Functions.hh:[[@LINE+2]]:17: remark: unexported public interface 'method_4'
   [[nodiscard]]
+  /* comment */ int method_4(int);
+
+  // CHECK: Functions.hh:[[@LINE+2]]:3: remark: unexported public interface 'method_5'
+  [[nodiscard]]
+  int method_5(int);
+
+  // CHECK: Functions.hh:[[@LINE+3]]:3: remark: unexported public interface 'method_6'
+  [[nodiscard]] /* two line
+                   comment */
   int method_6(int);
 
-  // CHECK: Functions.hh:[[@LINE+3]]:3: remark: unexported public interface 'method_7'
-  [[nodiscard]] [[deprecated("this function is deprecated")]] /* two line
-                                                                 comment */
+  // CHECK: Functions.hh:[[@LINE+2]]:3: remark: unexported public interface 'method_7'
+  [[nodiscard]] // this is a comment
   int method_7(int);
 
-  // CHECK: Functions.hh:[[@LINE+2]]:3: remark: unexported public interface 'method_8'
+  // CHECK: Functions.hh:[[@LINE+2]]:49: remark: unexported public interface 'method_8'
   [[nodiscard]] // this is a comment
-  int method_8(int);
-
-  // CHECK: Functions.hh:[[@LINE+2]]:60: remark: unexported public interface 'method_9'
-  [[nodiscard]] // this is a comment
-  __attribute((deprecated("this function is deprecated"))) int method_9(int);
+  [[deprecated("this function is deprecated")]] int method_8(int);
 };
